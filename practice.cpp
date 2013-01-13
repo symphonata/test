7c86380cd2558beb0019657708545681edb6b490
symphonata <symphonata@163.com>
2013-01-13 16:10:16 +0800

    add tags

--- practice.cpp
+++ practice.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <functional>
 
-int main(int argc, const char** argv) {
-	std::cout << "test" << std::endl;
+void genInts(std::function<void(int)> handle) {
+  for (int i = 0; i < 10; ++i)
+    handle(i);
+}
+
+void printNum(const char* prefix, int i) {
+  std::cout << prefix << " " << i << std::endl;
+}
+
+int main() {
+  using namespace std::placeholders;
+  genInts(std::bind(printNum, "The num is", _1));
 	return 0;
 }
 
