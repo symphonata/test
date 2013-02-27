24a7b47ed5dacb1230d792f266ea1abaaa8fa429
liuli <liuli@vtcentos.(none)>
2013-02-27 21:05:41 +0800

    add pthread test

--- Makefile
+++ Makefile
@@ -1,14 +1,14 @@
 CXXFLAGS += -Wall -g3 -std=c++0x
+CXXLFLAGS += -lpthread
 TARGET = practice
 
 .PHONY: clean tag
 
 $(TARGET): practice.o
-	$(CXX) $^ -o $@
+	$(CXX) $^ -o $@ $(CXXLFLAGS)
 
 practice.o: practice.cpp
-	$(CXX) -c $(CXXFLAGS) $^ -o $@
-
+	$(CXX) -c $(CXXFLAGS) $^ -o $@ 
 clean:
 	@rm -vf $(TARGET)
 	@rm -vf *.o
--- practice.cpp
+++ practice.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <functional>
+#include <pthread.h>
 
 void genInts(std::function<void(int)> handle) {
   for (int i = 0; i < 10; ++i)
@@ -10,9 +11,35 @@ void printNum(const char* prefix, int i) {
   std::cout << prefix << " " << i << std::endl;
 }
 
-int main() {
+void* thr_func(void* args)
+{
+  usleep(5000000);
+  long long n = reinterpret_cast<long long>(args);
+  pthread_t tid = pthread_self();
+  std::cout << "thread:" << n << " tid:" << tid << " run" << std::endl;
+  return (void*) 0;
+}
+
+void pthreadTest()
+{
+  for (int i = 0; i < 200; ++i)
+  {
+    pthread_t tidp;
+    int err = pthread_create(&tidp, NULL, thr_func, (void*)i);
+    if (err)
+      std::cout << "error:" << err << std::endl;
+  }
+}
+
+void bindTest()
+{
   using namespace std::placeholders;
   genInts(std::bind(printNum, "The num is", _1));
+}
+
+int main() {
+  pthreadTest();
+  usleep(10000000);
 	return 0;
 }
 
